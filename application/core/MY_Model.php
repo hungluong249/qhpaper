e13d9490ac9e26629d6fe0ee4a42ccba26eb3951
minhtruongmato <truong.do@matocreative.vn>
2018-08-08 16:57:22 +0700

    [done] implement color function in admin

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -9,6 +9,59 @@ class MY_Model extends CI_Model {
         parent::__construct();
     }
 	
+	public function insert($data) {
+        $this->db->set($data)->insert($this->table);
+
+        if ($this->db->affected_rows() == 1) {
+            return $this->db->insert_id();
+        }
+        return false;
+    }
+
+    public function update($id, $data) {
+        $this->db->where('id', $id);
+
+        return $this->db->update($this->table, $data);
+    }
+
+    public function fetch_all_pagination($limit = NULL, $start = NULL, $keywords = '', $or_like = true) {
+        $this->db->select($this->table .'.*');
+        $this->db->from($this->table);
+        $this->db->group_start();
+        $this->db->like('title', $keywords);
+        if($or_like == true){
+            $this->db->or_like('hexcolor', $keywords);
+        }
+        $this->db->group_end();
+        $this->db->where('is_deleted', 0);
+        $this->db->limit($limit, $start);
+        $this->db->order_by('id', 'desc');
+
+        return $result = $this->db->get()->result_array();
+    }
+
+    public function count_search($keywords = '', $or_like = true){
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->group_start();
+        $this->db->like('title', $keywords);
+        if($or_like == true){
+            $this->db->or_like('hexcolor', $keywords);
+        }
+        $this->db->group_end();
+        $this->db->where('is_deleted', 0);
+
+        return $result = $this->db->get()->num_rows();
+    }
+
+    public function get_by_id($id)
+    {
+    	$this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->where('id', $id);
+
+        return $result = $this->db->get()->row_array();
+    }
 
 }
 
---
+++ application/views/admin/property/list_property_view.php
@@ -0,0 +1,154 @@
+<!-- STYLE -->
+<!--
+<link rel="stylesheet" href="<?php echo site_url('assets/public/lib/') ?>dataTable/css/dataTables.bootstrap.min.css">
+-->
+<link rel="stylesheet" href="<?php echo site_url('assets/public/') ?>sass/admin/datatable.css">
+
+<!-- SCRIPT -->
+<!--
+<script src="<?php echo site_url('assets/public/lib/') ?>dataTable/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo site_url('assets/public/lib/') ?>dataTable/js/dataTables.bootstrap.min.js"></script>
+-->
+
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Bảng Màu
+            <small>Danh sách mã màu</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li><a href="#"><i class="fa fa-dashboard"></i> Bảng Màu</a></li>
+            <li class="active">Danh sách</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+            <?php if ($this->session->flashdata('message_error')): ?>
+                <div class="alert alert-warning alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <h4><i class="icon fa fa-warning"></i> Alert!</h4>
+                    <?php echo $this->session->flashdata('message_error'); ?>
+                </div>
+            <?php endif ?>
+            <?php if ($this->session->flashdata('message_success')): ?>
+                <div class="alert alert-success alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <h4><i class="icon fa fa-check"></i> Alert!</h4>
+                    <?php echo $this->session->flashdata('message_success'); ?>
+                </div>
+            <?php endif ?>
+            <div class="col-md-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title">Bảng Màu</h3>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="<?php echo base_url('admin/property/create') ?>" class="btn btn-primary" role="button">Thêm Mới</a>
+                        </div>
+                        <div class="col-md-6">
+                            <form action="<?php echo base_url('admin/property/index') ?>" method="get">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Tìm kiếm ..." name="search" value="<?php echo $keywords ?>">
+                                    <span class="input-group-btn">
+                                        <input type="submit" class="btn btn-block btn-primary" value="Tìm kiếm">
+                                    </span>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="table-responsive">
+                            <table id="table" class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>No.</th>
+                                        <th>Hình Ảnh</th>
+                                        <th>Tên Tính Chất</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php $i = 1 ?>
+                                    <?php foreach ($result as $key => $value): ?>
+                                        <tr class="remove_<?php echo $value['id'] ?>">
+                                            <td><?php echo $i++ ?></td>
+                                            <td><img src="<?php echo base_url('assets/upload/property/'. $value['image']) ?>" width="150"></td>
+                                            <td><?php echo $value['title'] ?></td>
+                                            <td>
+                                                <a href="<?php echo base_url('admin/property/edit/' . $value['id']) ?>" id="dataActionEdit"><i class="fa fa-pencil" aria-hidden="true"></i> </a>
+                                                <a href="javascript:void(0)" class="btn-remove" data-url="<?php echo base_url('admin/property/remove'); ?>" data-id="<?php echo $value['id'] ?>">
+                                                    <i class="fa fa-remove" aria-hidden="true"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach ?>
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th>No.</th>
+                                        <th>Hình Ảnh</th>
+                                        <th>Tên Tính Chất</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+        <!-- END ACCORDION & CAROUSEL-->
+    </section>
+</div>
+<script type="text/javascript">
+    $('.btn-remove').click(function(){
+        var url = $(this).data('url');
+        var id = $(this).data('id');
+
+        if(confirm('Chắc chắn xóa?')){
+            $.ajax({
+                method: "get",
+                url: url,
+                data: {
+                    id : id
+                },
+                success: function(response){
+                    if(response.status == 200){
+                        $('.remove_' + id).fadeOut();
+                    }
+
+                },
+                error: function(jqXHR, exception){
+                    console.log(errorHandle(jqXHR, exception));
+                }
+            });
+        }
+    });
+</script>
+<!-- DataTables -->
+<!--<script>-->
+<!--    $(function () {-->
+<!--        $('#table').DataTable({-->
+<!--            'paging'      : true,-->
+<!--            'lengthChange': false,-->
+<!--            'searching'   : false,-->
+<!--            'ordering'    : true,-->
+<!--            'info'        : true,-->
+<!--            'autoWidth'   : false-->
+<!--        })-->
+<!--    })-->
+<!--</script>-->
